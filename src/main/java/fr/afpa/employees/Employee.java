8c097ffda4d9c4a2db935e5ca6dc6bf493249f80
3nz0k <e.mrtnez7@gmail.com>
2026-03-03 15:26:00 +0100

    First Commit

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -9,6 +9,8 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
+import java.time.LocalDate;
+
 class Employee
 {
 	/**
@@ -16,11 +18,21 @@ class Employee
 	 */
 	private String registrationNumber;
 
+
 	// TODO compléter les attributs comme présenté dans le PDF
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialRate = 30;
+	private LocalDate birthDate;
 
 	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, LocalDate birthDate) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDate = birthDate;
 	}
 
 	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
@@ -34,6 +46,64 @@ class Employee
 
 	// TODO ajouter les getters et les setters
 
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getSocialRate() {
+		return socialRate;
+	}
+
+	public String getFullName()
+	{
+		return this.lastName + " " + this.firstName;
+	}
+
+	public double netSalary()
+	{
+		return this.salary - this.salary * this.socialRate /100;
+	}
+
+	public int daysBeforeBirthday(LocalDate date)
+	{
+		int dayEmployee = this.birthDate.getDayOfYear();
+		int dayNow = date.getDayOfYear();
+		if (dayEmployee < dayNow)
+		{
+			dayEmployee +=365;
+		}
+		return dayEmployee - dayNow;
+	}
+
 	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
 	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	@Override
+	public String toString() {
+		return "Registration number : " + registrationNumber +
+				" Last Name : " + lastName +
+				" First Name : " + firstName +
+				" Salary : " + salary +
+				" Social Rate : " + socialRate +
+				" BirthDate : " + birthDate;
+	}
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -1,7 +1,6 @@
 package fr.afpa.employees;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 /**
  * Classe principale du projet, contient la fonction "main"
@@ -13,9 +12,21 @@ class EmployeeMain
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
 		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		Employee employee1 = new Employee("11ABC22", "LAST", "Loji", 1200, LocalDate.parse("2003-03-04"));
+		Employee employee2 = new Employee("YBUHBDS", "Maedsq", "Ohuyi", 1678, LocalDate.parse("1992-12-31"));
+		Employee employee3 = new Employee("dsqdsqdz", "Dsdqzd", "Juyyd", 9278, LocalDate.parse("1932-06-12"));
+		Employee employee4 = new Employee("dzdsdf", "BHyud", "Dgrd", 4356, LocalDate.parse("1979-09-28"));
 
+
+		LocalDate date = LocalDate.now();
 		// TODO afficher les informations des employés avec System.out.println
+		System.out.println(date);
+		System.out.println(employee1);
+		System.out.println(employee1.netSalary());
+		System.out.println(employee2.daysBeforeBirthday(date));
+		System.out.println(employee2);
+		System.out.println(employee3);
+		System.out.println(employee4);
 	}
 }
 
